8ec64f4810b73882d6f7df7d0b1f9ab48b9872fb
Jonathan Woodard <jvw.226@gmail.com>
2022-11-25 09:28:49 -0600

    installed tailwind css to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter} from "react-router-dom"
 import { myBizCard } from "./myBizCard"
 
 const container = document.getElementById("root")
