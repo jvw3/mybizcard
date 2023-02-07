caefa201a79b6c22b4110289fe0a2aaf69664b11
Jonathan Woodard <jvw.226@gmail.com>
2023-02-07 14:48:51 -0600

    setup app structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { BrowserRouter} from "react-router-dom"
-import { myBizCard } from "./myBizCard"
+import { MyBizCard } from "./myBizCard"
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
     <BrowserRouter>
-        <myBizCard />
+        <MyBizCard />
     </BrowserRouter>
 )
