891228bad1024993aa7e591598d4f8cf60238413
Jonathan Woodard <jvw.226@gmail.com>
2023-02-07 14:49:05 -0600

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-     "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        30: "30vh",
+        20: "20vh",
+        25: "25vh",
+        75: "75vh",
+        40: "40vh",
+        80: "80vh",
+      },
+      width: {
+        30: "30vw",
+        75: "75vw",
+        40: "40vw",
+        80: "80vw",
+      },
+      backgroundImage: {
+        profilePic: "url(../src/images/profilepic.jpeg)"
+      },
+    },
   },
   plugins: [],
-}
+};
