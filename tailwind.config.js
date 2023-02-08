7d959c90f36aef8b322b691d8647d90422be744c
Jonathan Woodard <jvw.226@gmail.com>
2023-02-08 12:50:45 -0600

    tailwind updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens: {
+        "xxs-mobile": { raw: "(min-height: 780px),(max-height: 819px)" },
+        "xs-mobile": { raw: "(min-height: 820px),(max-height: 900px)" },
+        "large-mobile": { raw: "(min-height: 840px)" },
+        // => @media (max-height: 1234px) { ... }
+      },
       height: {
         30: "30vh",
         20: "20vh",
@@ -17,8 +23,27 @@ module.exports = {
         40: "40vw",
         80: "80vw",
       },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        quicksand: ["Quicksand, sans-serif"],
+        spacemono: ["Spacemono, sans-serif"],
+      },
       backgroundImage: {
-        profilePic: "url(../src/images/profilepic.jpeg)"
+        profilePic: "url(../src/images/profilepic.jpeg)",
+      },
+      keyframes: {
+        slideUp: {
+          "0%": { transform: "translateY(100%)" },
+          "100%": { transform: "translateY(0%)" },
+        },
+        slideDown: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(100%)" },
+        },
+      },
+      animation: {
+        slideUp: "slideUp 1s ease-in-out",
+        slideDown: "slideDown 1s ease-in-out",
       },
     },
   },
