3e5f6382095e4f04a75fa9e289f606280bfabc71
Jonathan Woodard <jvw.226@gmail.com>
2023-02-08 12:51:15 -0600

    refactored about me page

--- src/card/AboutMe.js
+++ src/card/AboutMe.js
@@ -1,11 +1,12 @@
 
 
-export const AboutMe = () => {
+export const AboutMe = ({setAboutMeVisibility, aboutMeVisible}) => {
   return (
     <>
-      <main className="bg-black">
-        <div className="m-4">
-          <p className="text-white">
+      <main className="">
+        <button onClick={() => setAboutMeVisibility(!aboutMeVisible) } className="absolute top-2 left-2 text-white text-3xl">X</button>
+        <div className="ml-4 mr-4 mt-20">
+          <p className="text-white text-xl">
             I'm a self-motivated, team oriented, algorithmic thinker with a
             passion for learning, and a strength for working in a high pressure,
             fast-paced, environment. I've recently made the transition to
