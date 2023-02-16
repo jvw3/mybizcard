b280599e8f19242c6806158a4db4a11a1dba863d
Jonathan Woodard <jvw.226@gmail.com>
2023-02-16 13:14:44 -0600

    updated about me section

--- src/card/AboutMe.js
+++ src/card/AboutMe.js
@@ -3,16 +3,15 @@
 export const AboutMe = ({setAboutMeVisibility, aboutMeVisible}) => {
   return (
     <>
-      <main className="">
-        <button onClick={() => setAboutMeVisibility(!aboutMeVisible) } className="absolute top-2 left-2 text-white text-3xl">X</button>
-        <div className="ml-4 mr-4 mt-20">
-          <p className="text-white text-xl">
+      <main className=" bg-indigo-700 rounded-2xl ">
+        <div className=" p-4">
+          <p className="text-white text-xs">
             I'm a self-motivated, team oriented, algorithmic thinker with a
             passion for learning, and a strength for working in a high pressure,
             fast-paced, environment. I've recently made the transition to
             software development after playing professional football for the
             last seven years. I'm accustomed to comprehending a variety of new
-            concepts in a short period of time and seeking a position to expand
+            concepts in a short period of time and I'm seeking a position to expand
             and leverage my strengths.
           </p>
         </div>
