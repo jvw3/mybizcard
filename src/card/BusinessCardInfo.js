1d3e6a50b9558a15c9d26c76851e2867933c400b
Jonathan Woodard <jvw.226@gmail.com>
2023-02-16 13:15:38 -0600

    refactored business card to support swipeable elements

--- src/card/BusinessCardInfo.js
+++ src/card/BusinessCardInfo.js
@@ -1,36 +1,88 @@
 import { useEffect } from "react";
 import mylogo from "../images/jwlogo.svg";
 import { Skills } from "./Skills";
-import { IconMail, IconExternalLink, IconBrandGithub } from "@tabler/icons-react";
+import {
+  IconMail,
+  IconExternalLink,
+  IconBrandGithub,
+} from "@tabler/icons-react";
+import { motion } from "framer-motion";
 import { SwiperContainer } from "./SwiperContainer";
 
 export const BusinessCardInfo = () => {
+  function openEmail() {
+    window.open(`mailto:jvw.226@gmail.com`);
+  }
+  function openGithub() {
+    window.open(`https://github.com/jvw3`);
+  }
   return (
     <>
-      <main className="">
-        <div className=" flex-col">
+      <main className="w-full">
+        <div className="">
           <div className="flex justify-evenly">
-            <button className="mt-5 bg-indigo-700 text-white p-3 rounded-xl text-xl w">
-              <div className="flex space-x-2">
-                <div>Email</div>
-                <IconMail />
-              </div>
-            </button>
-            <button className="mt-5 bg-indigo-700 text-white p-3 rounded-xl text-xl w">
-              <div className="flex space-x-2">
-                <div>Github</div>
-                <IconBrandGithub />
-              </div>
-            </button>
-            <button className="mt-5 bg-indigo-700 text-white p-3 rounded-xl text-xl">
-              <div className="flex space-x-2">
-                <div>Portfolio</div>
-                <IconExternalLink />
-              </div>
-            </button>
+            <motion.div
+              initial={{ scale: 0, rotation: -180 }}
+              animate={{ rotate: 0, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 10,
+                delay: 0.2,
+              }}
+              whileTap={{ scale: 0.8 }}
+              className="mt-5 bg-indigo-700 text-white p-3 rounded-xl text-xl shadow-lg shadow-indigo-700"
+            >
+              <button onClick={openEmail}>
+                <div className="flex space-x-2">
+                  <div>Email</div>
+                  <IconMail />
+                </div>
+              </button>
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0, rotation: -180 }}
+              animate={{ rotate: 0, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 10,
+                delay: 0.2,
+              }}
+              whileTap={{ scale: 0.8 }}
+              className="mt-5 bg-indigo-700 text-white p-3 rounded-xl text-xl shadow-lg shadow-indigo-700"
+            >
+              <button onClick={openGithub}>
+                <div className="flex space-x-2">
+                  <div>Github</div>
+                  <IconBrandGithub />
+                </div>
+              </button>
+            </motion.div>
+            <motion.div
+              initial={{ scale: 0, rotation: -180 }}
+              animate={{ rotate: 0, scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 50,
+                damping: 10,
+                delay: 0.2,
+              }}
+              className="mt-5 bg-indigo-700 text-white p-3 rounded-xl text-xl shadow-lg shadow-indigo-700"
+              whileTap={{ scale: 0.8 }}
+            >
+              <button>
+                <div className="flex space-x-2">
+                  <div>Portfolio</div>
+                  <IconExternalLink />
+                </div>
+              </button>
+            </motion.div>
           </div>
         </div>
-        <SwiperContainer />
+        <div className="w-full">
+          <SwiperContainer />
+        </div>
       </main>
     </>
   );
--- src/card/MyCard.js
+++ src/card/MyCard.js
@@ -4,6 +4,7 @@ import { CardInfo } from "./CardInfo";
 import { useState } from "react";
 import { BusinessCardInfo, Skills } from "./BusinessCardInfo";
 import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconBrandTwitter } from "@tabler/icons-react";
+import { motion } from "framer-motion"
 
 export const MyCard = () => {
   const [aboutMeVisible, setAboutMeVisibility] = useState(false);
@@ -13,52 +14,46 @@ export const MyCard = () => {
     <>
       <main className=" w-screen h-screen bg-indigo-900 font-roboto">
         <div className="flex-col">
-          <div className="relative flex items-start justify-center bg-headshotBackground bg-cover h-55">
-            <div className="bg-gradient-to-b from-transparent to-indigo-900 w-full h-full bg-opacity-40 flex-col ">
-              <div className="absolute bottom-0 text-white ml-2 mb-2">
-                <h1 className="text-2xl">Jonathan Woodard</h1>
-                <h2 className="text-xl">Software Developer</h2>
-              </div>
-              <div className="flex flex-col items-end absolute bottom-8 right-5 space-y-5">
-                <div className="flex space-x-3">
-                <button className=" bg-white rounded-full p-1 text-indigo-700 w-fit flex">
-                  <IconBrandTwitter />
-                </button>
-
+          <div className="relative flex items-start justify-center ">
+            <div className=" w-full h-full flex-col ">
+              <div className="bg-headshotBackground bg-cover h-30 bg-[center_top_-6rem] ">
+                <div className="bg-gradient-to-b from-transparent via-transparent to-indigo-900 w-full h-full bg-opacity-40">
+                  <div className="absolute bottom-0 text-white ml-2 mb-2 ">
+                    <h1 className="text-2xl">Jonathan Woodard</h1>
+                    <h2 className="text-xl">Software Developer</h2>
+                  </div>
                 </div>
-                <button className="bg-white rounded-full p-1 text-indigo-700 w-fit">
-                  <IconBrandInstagram />
+              </div>
+              <motion.div
+                initial={{ opacity: 0, translateX: 100 }}
+                animate={{ opacity: 1, translateX: 0 }}
+                transition={{ type: "spring", stiffness: 50, damping: 10 , delay: 0.1}}
+                className="flex flex-col items-end absolute bottom-6 right-4 space-y-5"
+              >
+                <div className="flex space-x-3">
+                  <a
+                    className="bg-white rounded-full p-1.5 text-indigo-700 w-fit flex"
+                    href="https://twitter.com/jonathanwoodard"
+                    target="_blank"
+                  >
+                    <IconBrandTwitter size={26} />
+                  </a>
+                </div>
+                <button className="bg-white rounded-full p-1.5 text-indigo-700 w-fit">
+                  <IconBrandInstagram size={26} />
                 </button>
-                <button
-                  className="bg-white rounded-full p-1 text-indigo-700 w-fit"
+                <a
+                  className="bg-white rounded-full p-1.5 text-indigo-700 w-fit"
                   href="https://www.linkedin.com/in/jonathan-woodard/"
                   target="_blank"
                 >
-                  <IconBrandLinkedin />
-                </button>
-              </div>
+                  <IconBrandLinkedin size={26} />
+                </a>
+              </motion.div>
             </div>
           </div>
-          <div className="flex justify-center">
-            {/* <CardInfo
-            setAboutMeVisibility={setAboutMeVisibility}
-            setAboutAnimation={setAboutAnimation}
-          /> */}
-          </div>
-          <div className="flex-col items-end text-center text-3xl bg-white rounded-tr-2xl rounded-tl-2xl h-45">
+          <div className="text-center  bg-white rounded-tr-2xl rounded-tl-2xl h-70">
             <BusinessCardInfo />
-          </div>
-          <div
-            className={`absolute ${
-              aboutMeVisible
-                ? "top-64 bg-black h-full animate-slideUp"
-                : "animate-slideDown bg-black -bottom-96 hidden"
-            }`}
-          >
-            <AboutMe
-              setAboutMeVisibility={setAboutMeVisibility}
-              aboutMeVisible={aboutMeVisible}
-            />
           </div>
         </div>
       </main>
