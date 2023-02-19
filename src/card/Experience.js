51fa03f909db094c3dc8d2a6f520de9768cdaa00
Jonathan Woodard <jvw.226@gmail.com>
2023-02-19 01:01:27 -0600

    created experience.js card

---
+++ src/card/Experience.js
@@ -0,0 +1,20 @@
+
+
+export const Experience = ({}) => {
+  return (
+    <>
+      <main className=" bg-indigo-700 rounded-2xl text-white">
+        <div className=" p-4">
+            <h1 className="text-md">Apprentice Software Developer</h1>
+            <h2 className="text-sm">Nashville Software School</h2>
+            <h2 className="text-sm">July 2022 - January 2023</h2>
+          <p className="text-white text-xs">
+            Intensive six-month (full-time) software development bootcamp with
+            immersive, lab-based learning focusing on HTML5, CSS3, Javascript,
+            React and server-side fundamentals with Python, Django and SQL.
+          </p>
+        </div>
+      </main>
+    </>
+  );
+};
